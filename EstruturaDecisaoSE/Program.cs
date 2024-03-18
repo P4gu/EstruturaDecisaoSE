7de6cb54b430098a3c971643ba562faf9ca00137
Suc1t <paulaf.guimaraes99@gmail.com>
2024-03-18 18:19:55 -0300

    Adicionar arquivos de projeto.

---
+++ EstruturaDecisaoSE/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstruturaDecisaoSE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Para utilizar os testes logicos precisamos dos:
+            Operadores relacionais:
+                Maior                   >
+                Menor                   <
+                Maior ou igual          >=
+                Menor ou igual          <=
+                NAO igual (diferente)   !=   
+
+            Operadores Logicos
+                "E"                     &&
+                "OU"                    ||
+            */
+
+            int idade;
+            Console.Write("informe sua idade:");
+            idade = int.Parse(Console.ReadLine());
+
+            if (idade >= 18) //Não se coloca ; na frente do if pois o ; encerra comando.
+            {
+                Console.WriteLine("Maior de idade");
+            }
+            else //Não tem teste logico.
+            {
+                Console.WriteLine("Menor de idade.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
